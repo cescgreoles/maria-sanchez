0798dde989273c557f65a8ea274488b99785fa54
cescgreoles <cesc.greoles@gmail.com>
2024-09-30 20:16:02 +0200

    projects

--- app/proyectos/page.tsx
+++ app/proyectos/page.tsx
@@ -63,6 +63,19 @@ import { promises as fs } from "fs";
 const Proyectos: React.FC = async () => {
   const years = await fs.readdir(process.cwd() + "/public/image");
 
+  const projects = await Promise.all(
+    years.map(async (year) => {
+      const result = await fs.readdir(process.cwd() + `/public/image/${year}`);
+      return {
+        [year]: result,
+      };
+    })
+  );
+
+  const yearProjects = projects.reduce((acc, curr) => {
+    return { ...acc, ...curr };
+  }, {});
+
   return (
     <div className="container mx-auto px-4 bg-black text-white">
       <header className="flex justify-between items-center py-8">
@@ -87,11 +100,7 @@ const Proyectos: React.FC = async () => {
       </header>
 
       <main className="mt-10">
-        {years.map(async (year) => {
-          const projects = await fs.readdir(
-            process.cwd() + `/public/image/${year}`
-          );
-
+        {Object.entries(yearProjects).map(async ([year, projects]) => {
           return (
             <section
               key={year}
@@ -101,23 +110,20 @@ const Proyectos: React.FC = async () => {
 
               <div className="flex-grow flex items-center justify-center">
                 <div className=" grid w-fit grid-cols-1 md:grid-cols-2 gap-x-16 ">
-                  {projects.map((project) => {
-                    console.log(`/image/${year}/${project}/1.JPG`);
-                    return (
-                      <div key={project} className="w-fit p-4 flex gap-2">
-                        <Image
-                          src={`/image/${year}/${project}/1.JPG`}
-                          alt={`Imagen del proyecto ${project} | ${year}`}
-                          className="w-100 h-32 mr-2" // Reducir el margen derecho si es necesario
-                          width={200}
-                          height={128}
-                        />
-                        {/* <p className="text-xl tracking-widest">
-                        {project < 10 ? `0${project}` : project}
-                      </p> */}
-                      </div>
-                    );
-                  })}
+                  {projects.map((project, i) => (
+                    <div key={project} className="w-fit p-4 flex gap-2">
+                      <Image
+                        src={`/image/${year}/${project}/1.JPG`}
+                        alt={`Imagen del proyecto ${project} | ${year}`}
+                        className="w-100 h-32 mr-2" // Reducir el margen derecho si es necesario
+                        width={200}
+                        height={128}
+                      />
+                      <p className="text-xl tracking-widest">
+                        {i < 9 ? `0${i + 1}` : i + 1}
+                      </p>
+                    </div>
+                  ))}
                 </div>
               </div>
             </section>
