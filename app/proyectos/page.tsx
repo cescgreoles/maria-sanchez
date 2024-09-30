2fc9414830021af529de65d974fc7f2e1ecf6e09
cescgreoles <cesc.greoles@gmail.com>
2024-09-30 20:05:27 +0200

    projects

--- app/proyectos/page.tsx
+++ app/proyectos/page.tsx
@@ -5,70 +5,66 @@ import Link from "next/link";
 import { FaTimes } from "react-icons/fa";
 import { promises as fs } from "fs";
 
-type Proyecto = {
-  id: number;
-  img: string;
-  descripcion: string;
-  photos?: string[];
-};
+// type Proyecto = {
+//   id: number;
+//   img: string;
+//   descripcion: string;
+//   photos?: string[];
+// };
 
-const proyectosPorAño: Record<number, Proyecto[]> = {
-  2019: [
-    {
-      id: 1,
-      img: "/photo1.JPG",
-      descripcion: "Descripción del proyecto A1",
-      photos: ["/image/2019/1/1.JPG"],
-    },
-    { id: 2, img: "/photo2.JPG", descripcion: "Descripción del proyecto A2" },
-    { id: 3, img: "/photo3.JPG", descripcion: "Descripción del proyecto A3" },
-    { id: 4, img: "/photo1.JPG", descripcion: "Descripción del proyecto A4" },
-    { id: 5, img: "/photo2.JPG", descripcion: "Descripción del proyecto A5" },
-    { id: 6, img: "/photo3.JPG", descripcion: "Descripción del proyecto A6" },
-  ],
-  2020: [
-    { id: 1, img: "/photo1.JPG", descripcion: "Descripción del proyecto B1" },
-    { id: 2, img: "/photo2.JPG", descripcion: "Descripción del proyecto B2" },
-    { id: 3, img: "/photo3.JPG", descripcion: "Descripción del proyecto B3" },
-    { id: 4, img: "/photo1.JPG", descripcion: "Descripción del proyecto B4" },
-    { id: 5, img: "/photo2.JPG", descripcion: "Descripción del proyecto B5" },
-    { id: 6, img: "/photo3.JPG", descripcion: "Descripción del proyecto B6" },
-  ],
-  2021: [
-    { id: 1, img: "/photo1.JPG", descripcion: "Descripción del proyecto C1" },
-    { id: 2, img: "/photo2.JPG", descripcion: "Descripción del proyecto C2" },
-    { id: 3, img: "/photo3.JPG", descripcion: "Descripción del proyecto C3" },
-    { id: 4, img: "/photo1.JPG", descripcion: "Descripción del proyecto C4" },
-    { id: 5, img: "/photo2.JPG", descripcion: "Descripción del proyecto C5" },
-    { id: 6, img: "/photo3.JPG", descripcion: "Descripción del proyecto C6" },
-  ],
-  2022: [
-    { id: 1, img: "/photo1.JPG", descripcion: "Descripción del proyecto D1" },
-    { id: 2, img: "/photo2.JPG", descripcion: "Descripción del proyecto D2" },
-    { id: 3, img: "/photo3.JPG", descripcion: "Descripción del proyecto D3" },
-    { id: 4, img: "/photo1.JPG", descripcion: "Descripción del proyecto D4" },
-    { id: 5, img: "/photo2.JPG", descripcion: "Descripción del proyecto D5" },
-    { id: 6, img: "/photo3.JPG", descripcion: "Descripción del proyecto D6" },
-  ],
-  2023: [
-    { id: 1, img: "/photo1.JPG", descripcion: "Descripción del proyecto E1" },
-    { id: 2, img: "/photo2.JPG", descripcion: "Descripción del proyecto E2" },
-    { id: 3, img: "/photo3.JPG", descripcion: "Descripción del proyecto E3" },
-    { id: 4, img: "/photo1.JPG", descripcion: "Descripción del proyecto E4" },
-    { id: 5, img: "/photo2.JPG", descripcion: "Descripción del proyecto E5" },
-    { id: 6, img: "/photo3.JPG", descripcion: "Descripción del proyecto E6" },
-  ],
-};
+// const proyectosPorAño: Record<number, Proyecto[]> = {
+//   2019: [
+//     {
+//       id: 1,
+//       img: "/photo1.JPG",
+//       descripcion: "Descripción del proyecto A1",
+//       photos: ["/image/2019/1/1.JPG"],
+//     },
+//     { id: 2, img: "/photo2.JPG", descripcion: "Descripción del proyecto A2" },
+//     { id: 3, img: "/photo3.JPG", descripcion: "Descripción del proyecto A3" },
+//     { id: 4, img: "/photo1.JPG", descripcion: "Descripción del proyecto A4" },
+//     { id: 5, img: "/photo2.JPG", descripcion: "Descripción del proyecto A5" },
+//     { id: 6, img: "/photo3.JPG", descripcion: "Descripción del proyecto A6" },
+//   ],
+//   2020: [
+//     { id: 1, img: "/photo1.JPG", descripcion: "Descripción del proyecto B1" },
+//     { id: 2, img: "/photo2.JPG", descripcion: "Descripción del proyecto B2" },
+//     { id: 3, img: "/photo3.JPG", descripcion: "Descripción del proyecto B3" },
+//     { id: 4, img: "/photo1.JPG", descripcion: "Descripción del proyecto B4" },
+//     { id: 5, img: "/photo2.JPG", descripcion: "Descripción del proyecto B5" },
+//     { id: 6, img: "/photo3.JPG", descripcion: "Descripción del proyecto B6" },
+//   ],
+//   2021: [
+//     { id: 1, img: "/photo1.JPG", descripcion: "Descripción del proyecto C1" },
+//     { id: 2, img: "/photo2.JPG", descripcion: "Descripción del proyecto C2" },
+//     { id: 3, img: "/photo3.JPG", descripcion: "Descripción del proyecto C3" },
+//     { id: 4, img: "/photo1.JPG", descripcion: "Descripción del proyecto C4" },
+//     { id: 5, img: "/photo2.JPG", descripcion: "Descripción del proyecto C5" },
+//     { id: 6, img: "/photo3.JPG", descripcion: "Descripción del proyecto C6" },
+//   ],
+//   2022: [
+//     { id: 1, img: "/photo1.JPG", descripcion: "Descripción del proyecto D1" },
+//     { id: 2, img: "/photo2.JPG", descripcion: "Descripción del proyecto D2" },
+//     { id: 3, img: "/photo3.JPG", descripcion: "Descripción del proyecto D3" },
+//     { id: 4, img: "/photo1.JPG", descripcion: "Descripción del proyecto D4" },
+//     { id: 5, img: "/photo2.JPG", descripcion: "Descripción del proyecto D5" },
+//     { id: 6, img: "/photo3.JPG", descripcion: "Descripción del proyecto D6" },
+//   ],
+//   2023: [
+//     { id: 1, img: "/photo1.JPG", descripcion: "Descripción del proyecto E1" },
+//     { id: 2, img: "/photo2.JPG", descripcion: "Descripción del proyecto E2" },
+//     { id: 3, img: "/photo3.JPG", descripcion: "Descripción del proyecto E3" },
+//     { id: 4, img: "/photo1.JPG", descripcion: "Descripción del proyecto E4" },
+//     { id: 5, img: "/photo2.JPG", descripcion: "Descripción del proyecto E5" },
+//     { id: 6, img: "/photo3.JPG", descripcion: "Descripción del proyecto E6" },
+//   ],
+// };
 
 const Proyectos: React.FC = async () => {
-  console.log("HOls");
-
-  const file = await fs.readFile(process.cwd() + "/public/data.json", "utf8");
-  const data = await JSON.parse(file);
+  const years = await fs.readdir(process.cwd() + "/public/image");
 
   return (
     <div className="container mx-auto px-4 bg-black text-white">
-      <p>{JSON.stringify(data)}</p>
       <header className="flex justify-between items-center py-8">
         <div className="flex items-center">
           <Image
@@ -91,33 +87,42 @@ const Proyectos: React.FC = async () => {
       </header>
 
       <main className="mt-10">
-        {Object.keys(proyectosPorAño).map((año) => (
-          <section
-            key={año}
-            className="mb-8 flex justify-between w-full h-full border-b-2 border-white "
-          >
-            <h2 className="text-2xl mb-4 mr-4">{año}</h2>
+        {years.map(async (year) => {
+          const projects = await fs.readdir(
+            process.cwd() + `/public/image/${year}`
+          );
 
-            <div className="flex-grow flex items-center justify-center">
-              <div className=" grid w-fit grid-cols-1 md:grid-cols-2 gap-x-16 ">
-                {proyectosPorAño[Number(año)].map((proyecto) => (
-                  <div key={proyecto.id} className="w-fit p-4 flex gap-2">
-                    <Image
-                      src={proyecto.img}
-                      alt={`Imagen del proyecto ${proyecto.id}`}
-                      className="w-100 h-32 mr-2" // Reducir el margen derecho si es necesario
-                      width={200}
-                      height={128}
-                    />
-                    <p className="text-xl tracking-widest">
-                      {proyecto.id < 10 ? `0${proyecto.id}` : proyecto.id}
-                    </p>
-                  </div>
-                ))}
+          return (
+            <section
+              key={year}
+              className="mb-8 flex justify-between w-full h-full border-b-2 border-white "
+            >
+              <h2 className="text-2xl mb-4 mr-4">{year}</h2>
+
+              <div className="flex-grow flex items-center justify-center">
+                <div className=" grid w-fit grid-cols-1 md:grid-cols-2 gap-x-16 ">
+                  {projects.map((project) => {
+                    console.log(`/image/${year}/${project}/1.JPG`);
+                    return (
+                      <div key={project} className="w-fit p-4 flex gap-2">
+                        <Image
+                          src={`/image/${year}/${project}/1.JPG`}
+                          alt={`Imagen del proyecto ${project} | ${year}`}
+                          className="w-100 h-32 mr-2" // Reducir el margen derecho si es necesario
+                          width={200}
+                          height={128}
+                        />
+                        {/* <p className="text-xl tracking-widest">
+                        {project < 10 ? `0${project}` : project}
+                      </p> */}
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
-            </div>
-          </section>
-        ))}
+            </section>
+          );
+        })}
       </main>
     </div>
   );
